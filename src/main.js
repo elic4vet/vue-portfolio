b06eb784f8f12842de34bdeb64ed68da2f537aa2
Elisabeth <elise.doisneau@gmail.com>
2024-09-12 20:53:37 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add()
+
+config.familyDefault = "classic";
+config.styleDefault = "duotone";
 
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
