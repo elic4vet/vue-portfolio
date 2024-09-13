0ee6fae0ca5db6063193b07145e629b934f3086a
Elisabeth <elise.doisneau@gmail.com>
2024-09-13 20:31:31 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAtom, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-library.add()
+library.add(faChevronRight);
+library.add(faAtom);
 
 config.familyDefault = "classic";
 config.styleDefault = "duotone";
