c4a6adb9296c40221e101f1c5dfe66d2b20ec58c
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 19:03:43 +0200

    Update EducationView.vue

--- src/views/EducationView.vue
+++ src/views/EducationView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="about">
+    <div class="education">
       <h1> Education page</h1>
     </div>
   </template>
   
   <style>
   @media (min-width: 1024px) {
-    .about {
+    .education {
       min-height: 100vh;
       display: flex;
       align-items: center;
