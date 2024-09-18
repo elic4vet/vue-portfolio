c7e658cd5db6df811b43550144b5e21198994013
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 20:09:26 +0200

    Update ProjectsView.vue

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,15 +1,24 @@
+
 <template>
-    <div class="projects">
-      <h1> My projects </h1>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .projects {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
+  <div id="projects">
+    <ul id="project_list">
+      <li><font-awesome-icon :icon="['fas', 'fa-star']" /> My portfolio with NextJs <a href="#"><font-awesome-icon :icon="['fas', 'chevron-right']" /></a></li>
+      <li><font-awesome-icon :icon="['fas', 'fa-star']" /> My portfolio with React <a href="#"><font-awesome-icon :icon="['fas', 'chevron-right']" /></a></li>
+      <li><font-awesome-icon :icon="['fas', 'fa-star']" /> An app built with Vue <a href="#"><font-awesome-icon :icon="['fas', 'chevron-right']" /></a></li>
+      <li><font-awesome-icon :icon="['fas', 'fa-star']" /> An app built with Angular <a href="#"><font-awesome-icon :icon="['fas', 'chevron-right']" /></a></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .projects {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: flex-start;
   }
-  </style>
+}
+</style>
+
+ 
