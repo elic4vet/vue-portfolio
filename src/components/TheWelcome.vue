266034d6d0d8f695e6b5aeb9b6eb37a0e3920a75
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 20:09:07 +0200

    Update TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,30 +3,17 @@ import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
+ 
       <DocumentationIcon />
+ 
     </template>
     <template #heading>Projects</template>
-
-    <div id="projects">
-      <ul id="project_list">
-        <li>My portfolio with NextJs <a href="#"><font-awesome-icon
-              :icon="['fas', 'chevron-right']" /></a></li>
-        <li>My portfolio with React <a href="#"><font-awesome-icon
-              :icon="['fas', 'chevron-right']" /></a></li>
-        <li>An app built with Vue <a href="#"><font-awesome-icon
-              :icon="['fas', 'chevron-right']" /></a></li>
-        <li>An app built with Angular <a href="#"><font-awesome-icon
-              :icon="['fas', 'chevron-right']" /></a></li>
-      </ul>
-    </div>
-
   </WelcomeItem>
 
   <WelcomeItem>
@@ -34,10 +21,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
       <ToolingIcon />
     </template>
     <template #heading>Experience</template>
-    <div class="experience">
-      <p> <img src="../assets/kreativstorm.png" class="logo"> Junior Frontend Developer training internship</p>
-      <p><img src="../assets/arvato.jpg" class="logo"> Arvato SE - Costumer service employee</p>
-    </div>
+ 
   </WelcomeItem>
 
   <WelcomeItem>
@@ -46,39 +30,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
     </template>
     <template #heading>Education</template>
     <div class="education">
-
-      <div><a src="../assets/codefirstgirlslogo.png"></a>
-        <p> Introduction to Python & Apps</p>
-        <p class="tags"> <button> Python </button>,<button> App Development </button>, <button> Pandas </button>,
-          <button>
-            Libraries
-          </button>
-        </p>
-      </div>
-
-      <div>
-        <a src="../assets/freecodecamplogo.png"></a>
-        <p> FreeCodeCamp - Responsive Web Design Certification </p>
-        <p class="tags"> <button>HTML </button>, <button> CSS </button> , <button> JavaScript</button> , <button>
-            React</button> , <button> TypeScript</button></p>
-      </div>
-
-      <a src="../assets/shecodeslogo.png"></a>
-      <p> SheCodes - Frontend Development Workshop </p>
-      <p class="tags"><button>HTML</button>, <button> CSS</button> , <button> JavaScript</button> , <button>
-          React</button> ,
-        <button>Figma</button>
-      </p>
-
-      <a src="../udacitylogo.png"></a>
-      <p> Udacity - Nanodegree program : AI Product Manager </p>
-      <p class="tags"><button> AI</button>, <button> Product Management</button> , <button> Machine learning</button>/>
-      </p>
-
-      <a src="../ekpalogo.jpg"></a>
-      <p> National and Kapodistrian University of Athens" - B.Sc. French Language and Litterature </p>
-      <p class="tags"><button> French</button>, <button> Language</button> , <button>
-          Litterature</button> ,<button> Bachelor Degree</button></p>
     </div>
   </WelcomeItem>
 </template>
