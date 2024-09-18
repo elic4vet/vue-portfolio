9cf0749d40b9fd1dcaddcb8fa2d9de416b1f27e7
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 19:02:18 +0200

    Create ResumeView.vue

---
+++ src/views/ResumeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="Resumé">
+      <h1>Resumé</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
