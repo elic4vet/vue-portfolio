28d86321db3f2cfba0e04342a625be731dbb9e71
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 19:03:51 +0200

    Update ProjectsView.vue

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="about">
+    <div class="projects">
       <h1> My projects </h1>
     </div>
   </template>
   
   <style>
   @media (min-width: 1024px) {
-    .about {
+    .projects {
       min-height: 100vh;
       display: flex;
       align-items: center;
