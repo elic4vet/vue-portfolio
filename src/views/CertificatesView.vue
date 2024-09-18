590572965c2e3b394c122236c2fc232eacb1a1b8
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 20:09:19 +0200

    Update CertificatesView.vue

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="certificates">
-      <h1>Certificates</h1>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .certificates {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
+  <div class="certificates">
+    <h1>Certificates</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .certificates {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: flex-start;
   }
-  </style>
+}
+</style>
