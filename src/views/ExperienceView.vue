fdbf21a484a6bfc47453df5bf2cd419728670407
Elisabeth <elise.doisneau@gmail.com>
2024-09-18 20:09:23 +0200

    Update ExperienceView.vue

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="experience">
-    <h1>Experience</h1>
+    <p> <img src="../assets/kreativstorm.png" class="logo"> Junior Frontend Developer training internship</p>
+    <p><img src="../assets/arvato.jpg" class="logo"> Arvato SE - Costumer service employee</p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .experience {
-    min-height: 100vh;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    align-items: start;
+    justify-content: flex-start;
+    gap: 20px;
+    padding: 10px;
   }
 }
 </style>
