10bfe6596ef7a9697e00e9c82576f7177b92beb9
Elisabeth <elise.doisneau@gmail.com>
2024-09-21 19:49:21 +0200

    Update TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -54,8 +54,8 @@ button {
 }
 
 .logo {
-  width: 3rem;
-  height: 3rem;
+  width: 3.8rem;
+  height: 3.8rem;
   border-radius: 10px;
   object-fit: cover;
   --tw-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
