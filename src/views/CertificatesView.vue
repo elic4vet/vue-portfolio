a974e964232004c9172041dfeab9572132287354
Elisabeth <elise.doisneau@gmail.com>
2024-09-21 20:33:46 +0200

    Update CertificatesView.vue

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -1,12 +1,88 @@
 <template>
   <div class="certificates">
-    <h1>Certificates</h1>
+    <div class="section">
+      <img src="../assets/certificates/cfg-certificate.png" class="image">
+      <div class="content">
+        <p> CFG - Introduction to Python & Apps </p>
+        <div class="tags">
+          <button> Python</button>
+          <button> PIP </button>
+          <button> Pandas</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <img src="../assets/certificates/27576.png" class="image">
+      <div class="content">
+        <p> SheCodes - Introduction to coding </p>
+        <div class="tags">
+          <button> Front-end</button>
+          <button> JavaScript</button>
+          <button> CSS</button>
+          <button> HTML</button>
+          <button> Hosting</button>
+          <button> Git</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <img src="../assets/certificates/35845.png" class="image">
+      <div class="content">
+        <p> SheCodes - Web Development </p>
+        <div class="tags">
+          <button> Front-end</button>
+          <button> JavaScript</button>
+          <button> CSS</button>
+          <button> HTML</button>
+          <button> Hosting</button>
+          <button> Git</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <img src="../assets/certificates/49354.png" class="image">
+      <div class="content">
+        <p> SheCodes - Web Development </p>
+        <div class="tags">
+          <button> Front-end</button>
+          <button> JavaScript</button>
+          <button> CSS</button>
+          <button> HTML</button>
+          <button> Hosting</button>
+          <button> Git</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <img src="../assets/certificates/57726.png" class="image">
+      <div class="content">
+        <p> SheCodes - Web Development </p>
+        <div class="tags">
+          <button> Front-end</button>
+          <button> JavaScript</button>
+          <button> CSS</button>
+          <button> HTML</button>
+          <button> Hosting</button>
+          <button> Git</button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
+
+  .image {
+    width: 200px;
+  }
   .certificates {
+    margin-top: 20px;
     display: flex;
     flex-direction: column;
     align-items: start;
