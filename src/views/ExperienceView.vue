0ae7b8e89a1dc0dd4f0458cf27c40a0b217359c5
Elisabeth <elise.doisneau@gmail.com>
2024-09-21 20:33:50 +0200

    Update ExperienceView.vue

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="experience">
-    <p> <img src="../assets/kreativstorm.png" class="logo"> Junior Frontend Developer training internship</p>
-    <p><img src="../assets/arvato.jpg" class="logo"> Arvato SE - Costumer service employee</p>
+
+    <div class="section">
+      <img src="../assets/kreativstorm.png" class="logo">
+      <div class="content">
+        <p> Junior Frontend Developer training internship</p>
+        <div class="tags">
+          <button> Front-end</button>
+          <button> JavaScript</button>
+          <button> Code review</button>
+          <button> GitHub</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+      <img src="../assets/arvato.jpg" class="logo">
+      <div class="content">
+        <p>Arvato SE - Costumer service employee</p>
+        <div class="tags">
+          <button> Logistics</button>
+          <button> Customer service</button>
+          <button> SAP</button>
+          <button> CRM</button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
- 
-
   p {
     margin: 0;
   }
@@ -16,10 +39,29 @@
   .experience {
     display: flex;
     flex-direction: column;
-    align-items: start;
-    justify-content: flex-start;
     gap: 20px;
     padding: 10px;
+  }
+
+  .section {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    align-items: flex-start;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    width: 100%;
+  }
+
+  .tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    margin-top: 10px;
   }
 }
 </style>
