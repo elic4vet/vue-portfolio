8d88c802386a42f373f1a94299bdd63f0f14c15b
Elisabeth <elise.doisneau@gmail.com>
2024-09-23 13:27:14 +0200

    Update ResumeView.vue

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="resume">
-    <h1>Resumé</h1>
+    <h1>Work Experience </h1>
     <div class="section">
       <img src="../assets/kreativstorm.png" class="logo">
       <div class="content">
@@ -11,8 +11,17 @@
     <div class="section">
       <img src="../assets/arvato.jpg" class="logo">
       <div class="content">
-        <h3>Arvato SE, Harsewinkel , DE — Customer Service Employee <br/>B2B, B2C (Markets : FR,CH, BNL, DE, EN)</h3>
+        <h3>Arvato SE, Harsewinkel , DE — Customer Service Employee <br />B2B, B2C (Markets : FR,CH, BNL, DE, EN)</h3>
         <span>OCT 2016 - today </span>
+      </div>
+    </div>
+
+    <div class="section">
+      <div class="content">
+        <a href="https://bit.ly/cv_eerkekoglou"
+          download>
+          <p> Download my CV here</p>
+        </a>
       </div>
     </div>
   </div>
