490ca617fc9d599ecc4dd0920227f481422a2095
Elisabeth <elise.doisneau@gmail.com>
2024-09-23 12:39:09 +0200

    Update ResumeView.vue

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="resume">
-    <h1>My Resume</h1>
+    <h1>Resumé</h1>
+    <div class="section">
+      <img src="../assets/kreativstorm.png" class="logo">
+      <div class="content">
+        <h3>Kreativstorm, Remote — Front End Development Intern</h3>
+        <span>JAN 2024 - FEB 2024</span>
+        <ul>
+          <li>Frontend Development: Participation in projects focused on frontend development technologies such as HTML,
+            CSS, JavaScript.</li>
+          <li>Project Collaborations: Collaboration with fellow interns on frontend development projects,
+            demonstrating teamwork and communication skills in a real-world project environment.</li>
+          <li>
+            Code Review Sessions: Involvement in code review sessions where interns provide feedback on each other's
+            frontend development projects, fostering collaboration and skill improvement.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="section">
+      <img src="../assets/arvato.jpg" class="logo">
+      <div class="content">
+        <h3>Arvato SE, Harsewinkel , DE — Customer Service Employee <br/>B2B, B2C (Markets : FR,CH, BNL, DE, EN)</h3>
+        <span>OCT 2016 - today </span>
+        <ul>
+          <li>
+          Learns quickly new skills (SAP, CRM, ERP, etc.) and applies them to daily tasks to improve efficiency and Productivity.
+          </li>
+          <li>Offers friendly and efficient service to customers and handles challenging situations with ease.</li>
+          <li>Actively listens to customers' requests, confirming full understanding before addressing concerns.</li>
+          <li>Increases customer satisfaction by resolving issues.</li>
+          <li>Carries out day-to-day duties accurately and efficiently (order management, shipment control, audits, etc.).</li>
+          <li>Monitors the company’s inventory to keep stock levels and databases updated.</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
