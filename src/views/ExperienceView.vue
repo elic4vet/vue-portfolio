4bed228d7dbb98f504c537ace9327034a75ea148
Elisabeth <elise.doisneau@gmail.com>
2024-09-23 13:13:16 +0200

    Update ExperienceView.vue

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -4,26 +4,50 @@
     <div class="section">
       <img src="../assets/kreativstorm.png" class="logo">
       <div class="content">
-        <p> Junior Frontend Developer training internship</p>
+        <h3> Junior Frontend Developer training internship</h3>
+        <span>JAN 2024 - FEB 2024</span>
         <div class="tags">
           <button> Front-end</button>
           <button> JavaScript</button>
           <button> Code review</button>
           <button> GitHub</button>
         </div>
+        <ul>
+          <li>Frontend Development: Participation in projects focused on frontend development technologies such as HTML,
+            CSS, JavaScript.</li>
+          <li>Project Collaborations: Collaboration with fellow interns on frontend development projects,
+            demonstrating teamwork and communication skills in a real-world project environment.</li>
+          <li>
+            Code Review Sessions: Involvement in code review sessions where interns provide feedback on each other's
+            frontend development projects, fostering collaboration and skill improvement.
+          </li>
+        </ul>
       </div>
     </div>
 
     <div class="section">
       <img src="../assets/arvato.jpg" class="logo">
       <div class="content">
-        <p>Arvato SE - Costumer service employee</p>
+        <h3>Arvato SE - Costumer service employee</h3>
+        <span>OCT 2016 - today </span>
         <div class="tags">
           <button> Logistics</button>
           <button> Customer service</button>
           <button> SAP</button>
           <button> CRM</button>
         </div>
+        <ul>
+          <li>
+            Learns quickly new skills (SAP, CRM, ERP, etc.) and applies them to daily tasks to improve efficiency and
+            Productivity.
+          </li>
+          <li>Offers friendly and efficient service to customers and handles challenging situations with ease.</li>
+          <li>Actively listens to customers' requests, confirming full understanding before addressing concerns.</li>
+          <li>Increases customer satisfaction by resolving issues.</li>
+          <li>Carries out day-to-day duties accurately and efficiently (order management, shipment control, audits,
+            etc.).</li>
+          <li>Monitors the company’s inventory to keep stock levels and databases updated.</li>
+        </ul>
       </div>
     </div>
 
