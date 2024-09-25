b01779261cd0fcc320bd293dc0c6504ea7dbf6bf
Elisabeth <elise.doisneau@gmail.com>
2024-09-25 12:00:47 +0200

    Update ResumeView.vue

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="resume">
-    <h1>Work Experience </h1>
-    <div class="section">
-      <img src="../assets/kreativstorm.png" class="logo">
-      <div class="content">
-        <h3>Kreativstorm, Remote — Front End Development Intern</h3>
-        <span>JAN 2024 - FEB 2024</span>
-      </div>
-    </div>
-    <div class="section">
-      <img src="../assets/arvato.jpg" class="logo">
-      <div class="content">
-        <h3>Arvato SE, Harsewinkel , DE — Customer Service Employee <br />B2B, B2C (Markets : FR,CH, BNL, DE, EN)</h3>
-        <span>OCT 2016 - today </span>
-      </div>
-    </div>
 
-    <div class="section">
-      <div class="content">
-        <a href="https://bit.ly/cv_eerkekoglou"
-          download>
-          <p> Download my CV here</p>
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
