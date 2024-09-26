6aca7a699fcc40f6d44cecff99acee0b99c89bdc
Elisabeth <elise.doisneau@gmail.com>
2024-09-26 20:09:29 +0200

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -7,21 +7,33 @@ import DocumentationIcon from '../components/icons/IconDocumentation.vue'
 
 <template>
   <WelcomeItem>
-    <template #icon><DocumentationIcon /></template>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
     <template #heading>Contact</template>
     <p> Feel free to contact me </p>
-    
+    <a href="https://www.linkedin.com/in/eerkekoglou/" class="link" target="_blank" rel="noreferrer"> <font-awesome-icon :icon="['fab', 'linkedin']" /></a>
+    <a href="https://github.com/elic4vet" class="link" target="_blank" rel="noreferrer"> <font-awesome-icon :icon="['fab', 'github']" /></a>
+    <a href= "mailto:elisabeth.erkekoglou@gmail.com" class="link"><font-awesome-icon :icon="['fas', 'envelope']" /> </a> 
   </WelcomeItem>
 
 </template>
 
-<style>
+<style scoped>
 @media (min-width: 1024px) {
   .contact {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+  }
+
+  .link {
+    font-size: 25px;
+  }
+
+  p {
+    font-size: 20px;
   }
 }
 </style>
